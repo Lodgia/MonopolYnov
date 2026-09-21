24c101d50da8571144d896f8dcc1c8e26fda8514
Tim <timothe.guichard@ynov.com>
2026-09-21 13:19:31 +0200

    [MODIF] modif de home pour acceuillir victoire

--- protected/home.tsx
+++ protected/home.tsx
@@ -1,10 +1,18 @@
 
+import { findWinner, type Player } from "../src/Player.ts";
+import { VictoryModal } from "../src/VictoryModal.tsx";
 
-export function Home() {
+interface HomeProps {
+    players?: Player[];
+}
+
+export function Home({ players = [] }: HomeProps) {
+    const winner = findWinner(players);
 
     return (
         <>
             <h1>You are on the home page !</h1>
+            <VictoryModal winner={winner} />
         </>
     )
 }
