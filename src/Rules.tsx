6449a2a946b9396d6908cdaed72b2d0bea40448c
Tim <timothe.guichard@ynov.com>
2026-09-22 14:51:48 +0200

    [MODIF] gestion de l'erreur 455]

--- src/Rules.tsx
+++ src/Rules.tsx
@@ -170,19 +170,3 @@ export default function Rules() {
         </>
     )
 }
-
-type props = {
-    isOpen: boolean; 
-    setIsOpen: (v: boolean) => void; 
-}
-
-function IaFound({ isOpen, setIsOpen } : props) {
-    return (
-        <>
-            <div className="fixed flex-col inset-0 z-50 flex items-center justify-center bg-black/80 p-4 backdrop-blur-sm" role="dialog" aria-modal="true" aria-labelledby="rules-title">
-                <p className='flex font-mono font-bold h-screen justify-center items-center text-[50px] text-white'>455 : IA Found</p>
-                <button onClick={() => setIsOpen(!isOpen) }>Revenir en arrière</button>
-            </div> 
-        </>
-    )
-}
