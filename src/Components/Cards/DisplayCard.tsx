1cc31e5e5efefe43a29861d9aa1354d0fab65bc9
baymeric <aymericbeaune917@gmail.com>
2026-09-22 16:20:30 +0200

    [ :rocket: ] - Modification couleur bg cards

--- src/Components/Cards/DisplayCard.tsx
+++ src/Components/Cards/DisplayCard.tsx
@@ -8,7 +8,7 @@ type props = {
 export default function DisplayCard({ cardLabel, cardType }: props) {
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 animate-fadeIn">
-            <div className={`p-3 rounded-[8px] ${cardType === "luckyCards" ? "bg-blue-500/60" : "bg-orange-500/60"} max-w-1/5`}>
+            <div className={`p-3 rounded-[8px] ${cardType === "luckyCards" ? "bg-orange-500/60" : "bg-blue-500/60"} max-w-1/5`}>
                 <div className="border-2 border-white p-3 rounded-[8px]">
                     <h1 className="text-center text-white/80 font-bold italic">{cardType === "luckyCards" ? "Chance" : "Communautaire"}</h1>
                     <div className="flex items-center gap-3">
